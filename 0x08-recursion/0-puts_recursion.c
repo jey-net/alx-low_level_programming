b8d3bcf57d8e4c7d14caeb686b8173bb05ea98a9
Princess <princessjanet1302@gmail.com>
2023-08-02 17:48:28 -0700

    lol

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#inclde "main.h"
+/**
+ * _puts_recursion - print a string
+ * @s:string
+ * Return:void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+{
+	_puts_recursion(s + 1);
+}
+else
+{
+	_putchar("\n");
+}
+} 
