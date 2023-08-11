e3544bd9365e81fe9b2a070bf1cc79e3f395ae49
Princess <princessjanet1302@gmail.com>
2023-08-10 18:20:36 -0700

    plea

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -39,7 +39,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	for (z = 0; z < x; z++)
 		s[z] = s1[z];
-	for(z = 0; z < y; z++)
+	for (z = 0; z < y; z++)
 		s[z + x] = s2[z];
 	s[x + y] = '\0';
 	return (s);
